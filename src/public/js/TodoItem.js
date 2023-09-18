2b8d4b1def888aa8b5128960f91285f598d738fb
bobubuntu <musgravehill@gmail.com>
2023-09-18 22:07:23 +0300

    component events

--- src/public/js/TodoItem.js
+++ src/public/js/TodoItem.js
@@ -2,7 +2,12 @@ export default {
     props: {
         todoItemProp: Object
     },
+    //emits: ['event-enlarge-text'],
+    //  props: ['todoItemProp'], 
     template: `
-    <li>{{ todoItemProp.text }}</li>
+    <li>
+    {{ todoItemProp.text }}
+    <button @click="$emit('event-enlarge-text')">Enlarge text</button>
+    </li>
     `
 }
