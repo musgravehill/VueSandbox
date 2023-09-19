d05ee1ccf236806dd64d74e78bfb0d0f06be3a18
bobubuntu <musgravehill@gmail.com>
2023-09-19 15:11:41 +0300

    SFC loader

---
+++ src/public/component/myComponent.vue
@@ -0,0 +1,21 @@
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'world!',
+            color: 'blue',
+        }
+    }
+}
+</script>
+
+<template>
+    <span class="example">{{ msg }}</span>
+</template>
+
+<style scoped>
+.example {
+    color: v-bind('color');
+}
+</style>
